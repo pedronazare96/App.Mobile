808b4222ff5e6d7dced8bc60f0641a945d0ceb34
Pedro Nazaré <85971674+pedronazare96@users.noreply.github.com>
2021-08-14 23:08:38 -0300

    Update ClienteDao.java

--- ClienteDao.java
+++ ClienteDao.java
@@ -1,16 +1,55 @@
 package com.alura.agenda.ui.activity.dao;
 
+import androidx.annotation.Nullable;
+
 import com.alura.agenda.ui.activity.Cliente;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ClienteDao {
 
-    private final static List<Cliente> clientes= new ArrayList<>();
+    private final static List<Cliente> clientes = new ArrayList<>();
+    private static int contadorDeIds = 1;
+
     public void salva(Cliente cliente) {
+        cliente.setId(contadorDeIds);
         clientes.add(cliente);
+        atualizaIds();
     }
 
-    public ArrayList<Cliente> todos() { return new ArrayList<>(clientes); }
+    private void atualizaIds() {
+        contadorDeIds++;
+    }
+
+    public void edita(Cliente cliente) {
+        Cliente clienteEncontrado = buscaClientePeloId(cliente);
+        if (clienteEncontrado != null) {
+            int posicaoDoCliente = clientes.indexOf(clienteEncontrado);
+            clientes.set(posicaoDoCliente, cliente);
+        }
+    }
+
+    @Nullable
+    private Cliente buscaClientePeloId(Cliente cliente) {
+        for (Cliente a : 
+                clientes) {
+            if (a.getId() == cliente.getId()) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public List<Cliente> todos() {
+        return new ArrayList<>(clientes);
+    }
+
+    public void remove(Cliente cliente) {
+        Cliente clienteDevolvido = buscaClientePeloId(cliente);
+        if(clienteDevolvido != null ) {
+            clientes.remove(clienteDevolvido);
+        }
+    }
 }
